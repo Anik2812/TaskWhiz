dc365c3da4ce550b948ea4381ab7215bfe2e3962
Anik2812 <anikshaikh28125@gmail.com>
2024-08-02 22:49:31 +0530

    functions update

--- app.py
+++ app.py
@@ -48,7 +48,8 @@ Talisman(app)
 cache = Cache(app, config={'CACHE_TYPE': 'simple'})
 
 # Setup rate limiting
-limiter = Limiter(app, key_func=get_remote_address)
+limiter = Limiter(key_func=get_remote_address)
+limiter.init_app(app)
 
 # Setup database connection (using SQLAlchemy as an example)
 from flask_sqlalchemy import SQLAlchemy
